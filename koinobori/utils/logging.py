001f1e22e819e17b2f6f674265a4362bd5c6a4f4
Andrew Clemons <andrew.clemons@gmail.com>
2024-10-19 14:16:28 +0900

    Use orjson for json logging with structlog.
    Tidy up logging configuration once again for best practices.

--- koinobori/utils/logging.py
+++ koinobori/utils/logging.py
@@ -1,46 +1,97 @@
+import contextlib
 import logging
-from typing import Literal
+import logging.config
+from typing import TYPE_CHECKING, Any, Literal
 
 import structlog
+
+if TYPE_CHECKING:
+    from structlog.typing import Processor
 
 
 def init_logging(mode: Literal["console", "json"] = "console") -> None:
     structlog.configure(
         processors=[structlog.stdlib.ProcessorFormatter.wrap_for_formatter],
+        context_class=dict,
         logger_factory=structlog.stdlib.LoggerFactory(),
         cache_logger_on_first_use=True,
     )
 
-    processors = [
+    processors: list[Processor] = [
         structlog.contextvars.merge_contextvars,
         structlog.processors.add_log_level,
+        structlog.stdlib.add_logger_name,
         structlog.processors.TimeStamper(fmt="iso", utc=True),
         structlog.stdlib.ProcessorFormatter.remove_processors_meta,
     ]
 
-    if mode == "console":
-        processors += [
-            structlog.processors.StackInfoRenderer(),
-            structlog.dev.set_exc_info,
-            structlog.dev.ConsoleRenderer(
-                exception_formatter=structlog.dev.rich_traceback,
-            ),
-        ]
-    elif mode == "json":
-        processors += [
-            structlog.processors.dict_tracebacks,
-            structlog.processors.JSONRenderer(),
-        ]
-    else:
-        msg = f"Unknown mode {mode}"
-        raise ValueError(msg)
+    match mode:
+        case "console":
+            processors.append(structlog.processors.StackInfoRenderer())
+            processors.append(structlog.dev.set_exc_info)
+
+            processors.append(
+                structlog.dev.ConsoleRenderer(
+                    exception_formatter=structlog.dev.rich_traceback
+                )
+            )
+        case "json":
+            processors.append(structlog.processors.dict_tracebacks)
+
+            def orjson_dumps(*args: Any, **kwargs: Any) -> str:
+                import orjson
+
+                kwargs["option"] = orjson.OPT_SORT_KEYS
+                return orjson.dumps(*args, **kwargs).decode()
+
+            def stdlib_dumps(*args: Any, **kwargs: Any) -> str:
+                import json
+
+                kwargs["sort_keys"] = True
+                return json.dumps(*args, **kwargs)
+
+            def safe_dumps(*args: Any, **kwargs: Any) -> str:
+                with contextlib.suppress(Exception):
+                    return orjson_dumps(*args, **kwargs)
+
+                with contextlib.suppress(Exception):
+                    return stdlib_dumps(*args, **kwargs)
+
+                # safe fallback
+                return repr(*args)
+
+            processors.append(structlog.processors.JSONRenderer(serializer=safe_dumps))
 
     formatter = structlog.stdlib.ProcessorFormatter(processors=processors)
 
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
 
+    add_handler(handler)
+
+
+def add_handler(handler: logging.Handler) -> None:
+    # clear logging configuration
+    logging.config.dictConfig({"version": 1, "disable_existing_loggers": False})
+
+    # clear all loggers set by anyone else
+    for name in logging.root.manager.loggerDict:
+        logger = logging.getLogger(name)
+        if not isinstance(logger, logging.PlaceHolder):
+            logger.setLevel(logging.NOTSET)
+            logger.handlers = []
+            logger.propagate = True
+
+    # set our root handler
     root_logger = logging.getLogger()
     root_logger.handlers.clear()
     root_logger.addHandler(handler)
-    root_logger.setLevel(logging.INFO)
+
+    # and finally apply our config
+    logging_config: dict[str, Any] = {
+        "loggers": {},
+        "version": 1,
+        "disable_existing_loggers": False,
+        "incremental": True,
+    }
+    logging.config.dictConfig(logging_config)
